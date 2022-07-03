9a72bb3f1b90016731d054b01aaa32110c62cece
Ankit Kumar <ankitkr0696@gmail.com>
2022-07-03 10:52:47 +0530

    download cv

--- components/admin/jobs/StudentApplied.js
+++ components/admin/jobs/StudentApplied.js
@@ -15,6 +15,38 @@ export default function StudentApplied({ token = '', id = '' }) {
     })
   }, [])
 
+  const getSelectedRowData = () => {
+    const selectedRows = gridRef.current.api.getSelectedRows()
+    let selectedData = selectedRows.map(
+      (node) => node.attributes.student.data.attributes.roll
+    )
+    selectedData = selectedData.toString()
+    downloadCV(selectedData)
+    return selectedData
+  }
+
+  const downloadCV = async (ids) => {
+    // download zip file
+    fetch(`${API_URL}/api/admin/resume-zip?rolls=${ids}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob)
+        const link = document.createElement('a')
+        link.href = url
+        link.setAttribute('download', 'resume.zip')
+        document.body.appendChild(link)
+        link.click()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   const query = qs.stringify(
     {
       populate: ['student.course', 'job.company', 'student.program'],
@@ -147,14 +179,13 @@ export default function StudentApplied({ token = '', id = '' }) {
             Export
           </button>
 
-          <Link href={`/admin/companies/add`}>
-            <button
-              type='button'
-              className='order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:order-1 sm:ml-3'
-            >
-              Download CV
-            </button>
-          </Link>
+          <button
+            onClick={getSelectedRowData}
+            type='button'
+            className='order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:order-1 sm:ml-3'
+          >
+            Download CV
+          </button>
         </div>
       </div>
 
