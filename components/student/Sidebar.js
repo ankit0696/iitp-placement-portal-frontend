61e153490bf06bcb96ce8fb8d9ace3df21c8f6f1
Ankit Kumar <ankitkr0696@gmail.com>
2022-07-10 23:05:49 +0530

    user image change

--- components/student/Sidebar.js
+++ components/student/Sidebar.js
@@ -172,7 +172,7 @@ export default function Sidebar({ heading = '', children }) {
                     <div>
                       <img
                         className='inline-block h-10 w-10 rounded-full'
-                        src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                        src='/user.png'
                         alt=''
                       />
                     </div>
@@ -260,7 +260,7 @@ export default function Sidebar({ heading = '', children }) {
                 <div>
                   <img
                     className='inline-block h-9 w-9 rounded-full'
-                    src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                    src='/user.png'
                     alt=''
                   />
                 </div>
