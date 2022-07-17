52dc5f690a00e097b456d1d3b05b4ccd4430f210
Ankit Kumar <ankitkr0696@gmail.com>
2022-07-17 12:32:17 +0530

    rejected

--- components/admin/jobs/StudentApplied.js
+++ components/admin/jobs/StudentApplied.js
@@ -350,7 +350,7 @@ export default function StudentApplied({ token = '', id = '' }) {
           <button
             type='button'
             onClick={handleRejected}
-            className='order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 sm:order-1 sm:ml-3'
+            className='order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:order-1 sm:ml-3'
           >
             Mark as Rejected
           </button>
