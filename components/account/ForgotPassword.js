53e1691d529c5398c59e949df9a9e77b6c5a71df
Ankit Kumar <ankitkr0696@gmail.com>
2022-08-18 19:23:26 +0530

    a1 A1 fix

--- components/account/ForgotPassword.js
+++ components/account/ForgotPassword.js
@@ -95,7 +95,7 @@ export default function ForgotPassword() {
                   type='submit'
                   className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                 >
-                  Send Request to Admin
+                  Get password reset email
                 </button>
               </div>
             </form>
