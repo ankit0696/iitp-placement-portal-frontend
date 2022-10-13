7b8785d124d0182e1511aec872a3e7f115be16b1
Ankit Kumar <ankitkr0696@gmail.com>
2022-10-13 08:28:13 +0530

    removed accept button

--- components/admin/requests/StudentRequest.js
+++ components/admin/requests/StudentRequest.js
@@ -153,20 +153,6 @@ export default function StudentRequest({ token = '' }) {
             Student Registration
           </h2>
         </div>
-        <div className='mt-4 flex md:mt-0 md:ml-4'>
-          <button
-            type='button'
-            className='inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500'
-          >
-            Accept
-          </button>
-          <button
-            type='button'
-            className='ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500'
-          >
-            Reject
-          </button>
-        </div>
       </div>
       <div className='ag-theme-alpine mt-4' style={{ height: 300 }}>
         <AgGridReact
