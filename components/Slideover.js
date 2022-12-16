ef0044e252e514dbb43c03d5cf880b50e48b642c
Ankit Kumar <ankitkr0696@gmail.com>
2022-12-17 01:32:10 +0530

    notification desc fix

--- components/Slideover.js
+++ components/Slideover.js
@@ -124,7 +124,7 @@ export default function Slideover({ open = false, setOpen }) {
                                   {notification.attributes.title}
                                 </p>
                                 {/* Notification description */}
-                                <p className='truncate text-sm text-gray-500'>
+                                <p className='text-sm text-gray-500'>
                                   {notification.attributes.description}
                                 </p>
 
