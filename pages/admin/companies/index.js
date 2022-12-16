0b4daa3cb430be752429efb290f6b890f8623278
Ankit Kumar <ankitkr0696@gmail.com>
2022-12-16 20:08:10 +0530

    fix for pagination and companies ordered desc

--- pages/admin/companies/index.js
+++ pages/admin/companies/index.js
@@ -72,26 +72,17 @@ export default function Students({ token }) {
       headers: { Authorization: `Bearer ${token}` },
     };
 
-    // Get all students, for strapi's pagination, using count of 50 per page
-    const PAGE_SIZE = 100;
 
-    axios.get(`${API_URL}/api/companies?pagination[page]=1&pagination[pageSize]=${PAGE_SIZE}&populate=*`, config)
-      .then(async res => {
-        let fetched_data = res.data.data;
-        let total_cnt = res.data.meta.pagination.total;
-
-        while (fetched_data.length < total_cnt) {
-          const res = await axios.get(`${API_URL}/api/companies?pagination[page]=${fetched_data.length/PAGE_SIZE + 1}&pagination[pageSize]=${PAGE_SIZE}&populate=*`, config);
-          fetched_data = fetched_data.concat(res.data.data);
-          fetched_data.length += res.data.meta.pagination.pageSize;
-        }
-
-        setRowData(fetched_data);
+    axios
+      .get(`${API_URL}/api/companies?populate=*&sort=id:DESC`, config)
+      .then(async (res) => {
+        let fetched_data = res.data.data
+        setRowData(fetched_data)
       })
-      .catch(err => {
-        toast.error("Error while fetching data");
-        console.error(err);
-      });
+      .catch((err) => {
+        toast.error('Error while fetching data')
+        console.error(err)
+      })
   }, [])
 
   return (
